5fa01987187ec1b11446aac3ec0c2a85c3af993d
drodriguezdcr <drodriguez.dcr@gmail.com>
2022-02-03 18:37:11 -0500

    Endpoint to get quiz questions

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mysql = require('mysql');
+
+require('dotenv').config();
+
+//Create connection
+const db = mysql.createConnection({
+  host : process.env.DB_HOST,
+  user : process.env.DB_USER,
+  password : process.env.DB_PASS,
+  database : process.env.DB_NAME
+});
+
+//Connect to db
+db.connect((err) => {
+  if(err) throw err;
+  console.log('MySQL Connected...');
+});
+
+const app = express();
+
+// Create DB
+app.get('/getquestions', (req, res) => {
+  let sql = "SELECT * FROM quiz_questions";
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    res.send(result);
+  });
+});
+
+app.listen('3000', () =>{
+
+});
